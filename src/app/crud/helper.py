531f96be2ff6ecf70dc9235dfac3c0549297d497
Igor Magalhaes <igor.magalhaes.r@gmail.com>
2023-11-21 03:11:15 -0300

    get_joined and get_multi_joined methods created

--- src/app/crud/helper.py
+++ src/app/crud/helper.py
@@ -1,4 +1,9 @@
-from typing import Any, List, Type, Union
+from typing import Any, List, Type, Union, Optional
+from sqlalchemy import inspect
+from sqlalchemy.orm import DeclarativeMeta
+from sqlalchemy.sql import ColumnElement
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.elements import Label
 
 from pydantic import BaseModel
 
@@ -53,3 +58,61 @@ def _extract_matching_columns_from_column_names(model: Type[Base], column_names:
             column_list.append(getattr(model, column_name))
 
     return column_list
+
+def _auto_detect_join_condition(base_model: Type[DeclarativeMeta], join_model: Type[DeclarativeMeta]) -> Optional[ColumnElement]:
+    """
+    Automatically detects the join condition for SQLAlchemy models based on foreign key relationships.
+    This function scans the foreign keys in the base model and tries to match them with columns in the join model.
+
+    Parameters
+    ----------
+    base_model : Type[DeclarativeMeta]
+        The base SQLAlchemy model from which to join.
+    join_model : Type[DeclarativeMeta]
+        The SQLAlchemy model to join with the base model.
+
+    Returns
+    -------
+    Optional[ColumnElement]
+        A SQLAlchemy ColumnElement representing the join condition, if successfully detected.
+
+    Raises
+    ------
+    ValueError
+        If the join condition cannot be automatically determined, a ValueError is raised.
+
+    Example
+    -------
+    # Assuming User has a foreign key reference to Tier:
+    join_condition = auto_detect_join_condition(User, Tier)
+    """
+    fk_columns = [col for col in inspect(base_model).c if col.foreign_keys]
+    join_on = next(
+        (base_model.__table__.c[col.name] == join_model.__table__.c[list(col.foreign_keys)[0].column.name]
+         for col in fk_columns if list(col.foreign_keys)[0].column.table == join_model.__table__),
+        None
+    )
+
+    if join_on is None:
+        raise ValueError("Could not automatically determine join condition. Please provide join_on.")
+
+    return join_on
+
+def _add_column_with_prefix(column: Column, prefix: Optional[str]) -> Label:
+    """
+    Creates a SQLAlchemy column label with an optional prefix.
+
+    Parameters
+    ----------
+    column : Column
+        The SQLAlchemy Column object to be labeled.
+    prefix : Optional[str]
+        An optional prefix to prepend to the column's name.
+
+    Returns
+    -------
+    Label
+        A labeled SQLAlchemy Column object.
+    """
+    column_label = f"{prefix}{column.name}" if prefix else column.name
+    return column.label(column_label)
