dae49490287fcc1e6defdfeef351d0007dd5ee7a
Igor Benav <igor.magalhaes.r@gmail.com>
2025-02-10 21:03:21 -0300

    fastcrud bump, dependencies fixed

--- src/app/core/db/crud_token_blacklist.py
+++ src/app/core/db/crud_token_blacklist.py
@@ -3,5 +3,5 @@ from fastcrud import FastCRUD
 from ..db.token_blacklist import TokenBlacklist
 from ..schemas import TokenBlacklistCreate, TokenBlacklistUpdate
 
-CRUDTokenBlacklist = FastCRUD[TokenBlacklist, TokenBlacklistCreate, TokenBlacklistUpdate, TokenBlacklistUpdate, None]
+CRUDTokenBlacklist = FastCRUD[TokenBlacklist, TokenBlacklistCreate, TokenBlacklistUpdate, TokenBlacklistUpdate, None, None]
 crud_token_blacklist = CRUDTokenBlacklist(TokenBlacklist)
--- src/app/crud/crud_users.py
+++ src/app/crud/crud_users.py
@@ -3,5 +3,5 @@ from fastcrud import FastCRUD
 from ..models.user import User
 from ..schemas.user import UserCreateInternal, UserDelete, UserUpdate, UserUpdateInternal
 
-CRUDUser = FastCRUD[User, UserCreateInternal, UserUpdate, UserUpdateInternal, UserDelete]
+CRUDUser = FastCRUD[User, UserCreateInternal, UserUpdate, UserUpdateInternal, UserDelete, None]
 crud_users = CRUDUser(User)
